b57d27e81419b2cf5ab4bfce288b73e0806d0076
Christian Engel <engel.christian1994@gmail.com>
2025-03-09 02:04:36 +0100

    Cleanup

--- grpc-example/src/types.rs
+++ grpc-example/src/types.rs
@@ -25,22 +25,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_proto_to_rust_conversion() {
-        let proto_track = proto::Track { id: 42 };
-        let rust_track: Track = proto_track.into();
-        let back_proto: proto::Track = rust_track.clone().into();
-        assert_eq!(rust_track, Track { id: TrackId(42) });
-        assert_eq!(proto_track, back_proto);
-    }
-
-    #[test]
-    fn test_rust_to_proto_conversion() {
-        let rust_track = Track { id: TrackId(123) };
-        let proto_track: proto::Track = rust_track.into();
-        assert_eq!(proto_track.id, 123);
-    }
-
-    #[test]
     fn test_round_trip() {
         let original_proto = proto::Track { id: 999 };
         let rust_track: Track = original_proto.clone().into();
